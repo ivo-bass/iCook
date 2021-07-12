778729180e2040955890af76752c9469cc208aec
ivo-bass <ivailo.ignatoff@gmail.com>
2021-07-13 00:33:18 +0300

    some css stuff

--- LetsCook/settings.py
+++ LetsCook/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = os.environ.get('KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    '4e691a3d8721.ngrok.io',
+]
 
 # Application definition
 
